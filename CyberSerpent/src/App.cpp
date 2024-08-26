27ce306b9dfd049a974374f4d71e772aeaf60a46
GLITCHRS <glitch@glitch.business>
2024-08-27 00:42:22 +0300

    correct sleep to work on linux too.

--- CyberSerpent/src/App.cpp
+++ CyberSerpent/src/App.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 #include "core/engine.h"
 
@@ -7,5 +9,5 @@ int main()
 	CS::Engine& engine{ CS::Engine::Get() };
 	engine.Run();
 
-	Sleep(2000);
+	std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 }
