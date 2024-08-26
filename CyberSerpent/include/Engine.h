f93558c6b8c7646b14100b4cd3024690297a7d8c
GLITCHRS <glitch@glitch.business>
2024-08-26 22:55:25 +0300

    create the tool engine with checking root privilege at start, add linux root check as well.

--- CyberSerpent/include/Engine.h
+++ CyberSerpent/include/Engine.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
 namespace CS
 {
 	class Engine
@@ -11,8 +17,36 @@ namespace CS
 	public:
 		void Run() const
 		{
-			std::cout << "Hello World\n";
+			if (IsRoot())
+				std::cout << "ROOT!\n";
+			else
+				std::cout << ":(\n";
 		}
+#ifdef _WIN32
+		bool IsRoot() const
+		{
+			BOOL isMember;
+			PSID administratorsGroup = nullptr;
+			SID_IDENTIFIER_AUTHORITY SIDAuthNT = SECURITY_NT_AUTHORITY;
+
+			if (AllocateAndInitializeSid(&SIDAuthNT, 2, SECURITY_BUILTIN_DOMAIN_RID,
+				DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &administratorsGroup))
+			{
+				if (CheckTokenMembership(nullptr, administratorsGroup, &isMember))
+				{
+					FreeSid(administratorsGroup);
+					return isMember != 0;
+				}
+				FreeSid(administratorsGroup);
+			}
+			return false;
+		}
+#else
+		bool IsRoot() const
+		{
+			return getuid() == 0;
+		}
+#endif
 
 	public:
 		static Engine& Get()
--- CyberSerpent/src/App.cpp
+++ CyberSerpent/src/App.cpp
@@ -6,4 +6,6 @@ int main()
 {
 	CS::Engine& engine{ CS::Engine::Get() };
 	engine.Run();
+
+	Sleep(2000);
 }
