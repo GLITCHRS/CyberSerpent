3ea4dbff5dbf63c7100984d6e129c7b35d8cea52
GLITCHRS <glitch@glitch.business>
2024-09-03 20:36:22 +0300

    fix some forgotten about stuff.

--- Shell/src/system/linux/Linux.cpp
+++ Shell/src/system/linux/Linux.cpp
@@ -5,7 +5,18 @@
 #include "pch/pch.h"
 #include <unistd.h>
 
-std::string Linux::ExecCommands(const std::string&) const
+static std::string FileToString(FILE* file)
+{
+	std::string result;
+	char buffer[128];
+
+	while (fgets(buffer, sizeof(buffer), file) != nullptr)
+		result += buffer;
+
+	return result;
+}
+
+std::string Linux::ExecCommands(const std::string& command) const
 {
 	std::string commandResult{ "" };
 	Command sysCommand{ m_SysCommands.at(command) };
@@ -25,13 +36,6 @@ std::string Linux::ExecCommands(const std::string&) const
 bool Linux::IsRoot() const
 {
 	return getuid() == 0;
-}
-
-void Linux::Clear() const
-{
-	FILE* pipe = popen("clear", "w");
-	if (pipe)
-		pclose(pipe);
 }
 
 Linux::Linux()
